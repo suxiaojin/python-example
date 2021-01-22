f3870d7d94aefc08f29867a07eb35d5f1f2293d1
suxiaojin <suxiaojin928380@126.com>
2021-01-22 10:23:00 +0800

    1-22

---
+++ python读取配置文件.py
@@ -0,0 +1,16 @@
+对配置文件进行增、删、改、查
+
+
+#1.读取 ini配置文件 config.ini
+[csdn.net]
+user=suxiaojin
+[51cto]
+passwd=7150231
+
+from configparser import ConfigParser
+cfg=ConfigParser()
+cfg.read('config.ini')
+cfg.sections() ----['csdn.net','51cto']
+cfg.get('csdn.net','user')    ---suxiaojin
+改
+cfg.set('csdn.net','user','sulingjun')
