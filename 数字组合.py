cda711432cc08372fed7e94cea0edfbf4d41aad0
suxiaojin <suxiaojin928380@126.com>
2021-01-27 17:57:12 +0800

    1-27

--- 数字组合.py
+++ 数字组合.py
@@ -26,3 +26,61 @@ for i in range(len(thresholds)):
         profit-=thresholds[i]
 bonus+=profit*rates[-1]
 print(bonus)
+
+#这是第几天
+def isleapYear(y):
+    return (y%400==0 or (y%4==0 and y%100!=0))
+Dofm=[0,31,28,31,30,31,30,31,31,30,31,30]
+res=0
+year=int(input('Year:'))
+month=int(input('month:'))
+day=int(input('day:'))
+if isleapYear(year):
+    Dofm[2]+=1
+    for i in range(month):
+        res+=Dofm[i]
+    print(res+day)
+
+#九九乘法表
+for i in range(1,10):
+    for j in range(1,i+1):
+        print('%d*%d=%2ld ' % (i, j, i * j), end='')
+    print()
+
+#1-100之和
+res=0
+for i in range(1,101):
+    res+=i
+print(res)
+
+#遍历列表
+
+l=['moyu','niupi','xuecaibichi','shengfaji','42']
+for i in range(len(l)):
+    print(l[i])
+
+#查找字典最大值
+person = {"li":91,"wang":50,"zhang":20,"sun":22,'su':90}
+m = 'li'
+for key in person.keys():
+    if person[m] < person[key]:
+        m = key
+print ('%s,%d' % (m,person[m]))
+
+#列表中求偶数的和
+a=[1,2,3,4,5,6]
+s=sum([num for num in a if num%2==0])
+print(s)
+
+#列表中删除元素
+a=[1,2,3,4,5]
+del a[1::2]
+print(a)
+
+#用空格分隔的整数到一个列表
+l=list(map(int,input().split()))
+print(l)
+
+#记数，某个值出现的次数
+import re
+print(len(re.findall('python','python is a programing language.python is python')))
